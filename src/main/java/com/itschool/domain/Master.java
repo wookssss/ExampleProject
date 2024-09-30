3cf74dfaceffdcb3f317bc021326e9ae61c9741d
Woo Jeongwook <dnwjddnr115@naver.com>
2024-09-30 16:08:31 +0900

    Master 클래스 생성

--- src/main/java/com/itschool/domain/Master.java
+++ src/main/java/com/itschool/domain/Master.java
@@ -1,2 +1,33 @@
-package com.itschool.domain;public class Master {
+package com.itschool.domain;
+
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Master {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(updatable = false)
+    private Long maseterKey;
+
+    @Column(nullable = false,unique = true)
+    private String maseterId;
+
+    @Column(nullable = false,unique = true)
+    private String buisnessNumber;
+
+    @Column(nullable = false,unique = true)
+    private String storeName;
+
+    @Column(nullable = false)
+    private String masterName;
+
+    @Column(nullable = false,unique = true)
+    private String callNumber;
+
 }
